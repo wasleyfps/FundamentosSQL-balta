3f5bcb81d6a7ef8db10ef220e7b37da59238a9cb
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2024-07-02 14:00:20 -0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -6,3 +6,16 @@ FROM
 --     [CategoriaId] = 1
 ORDER BY
     [Nome] DESC
+
+
+
+----------------------------------------------------------
+SELECT TOP 100
+    [Id], [Nome], [CategoriaId]
+FROM
+    [Curso]
+WHERE
+    [Id] = 1 OR
+    [CategoriaId] = 1 
+    -- Pode colocar também IS NOT NULL pra saber se uma categoria não é nula
+    -- <=, >=, !=, =, AND, OR
